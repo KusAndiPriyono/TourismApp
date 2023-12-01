a808b0a47c2aa6dfd049a85fd0e2c4cc58c2795c
KusAndiPriyono <andipriyono94@gmail.com>
2023-12-01 22:38:47 +0700

    Studi Kasus Dagger Hilt

--- app/src/main/java/com/bangkit/tourismapp/ui/detail/DetailTourismViewModel.kt
+++ app/src/main/java/com/bangkit/tourismapp/ui/detail/DetailTourismViewModel.kt
@@ -3,8 +3,12 @@ package com.bangkit.tourismapp.ui.detail
 import androidx.lifecycle.ViewModel
 import com.bangkit.tourismapp.core.domain.model.Tourism
 import com.bangkit.tourismapp.core.domain.usecase.TourismUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class DetailTourismViewModel(private val tourismUseCase: TourismUseCase) : ViewModel() {
+@HiltViewModel
+class DetailTourismViewModel @Inject constructor(private val tourismUseCase: TourismUseCase) :
+    ViewModel() {
 
     fun setFavoriteTourism(tourism: Tourism, newStatus: Boolean) =
         tourismUseCase.setFavoriteTourism(tourism, newStatus)
--- app/src/main/java/com/bangkit/tourismapp/ui/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/bangkit/tourismapp/ui/favorite/FavoriteViewModel.kt
@@ -3,8 +3,11 @@ package com.bangkit.tourismapp.ui.favorite
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import com.bangkit.tourismapp.core.domain.usecase.TourismUseCase
+import dagger.hilt.android.HiltAndroidApp
+import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+@HiltViewModel
 class FavoriteViewModel @Inject constructor(tourismUseCase: TourismUseCase) : ViewModel() {
 
     val favoriteTourism = tourismUseCase.getFavoriteTourism().asLiveData()
